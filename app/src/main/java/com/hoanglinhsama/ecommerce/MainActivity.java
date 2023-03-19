572307deaacbdca8b3f2d19f28a4a864a7d75e8d
Hoang Linh Sama <linhspdragonball@gmail.com>
2023-03-19 18:30:33 +0700

    Thiet ke giao dien man hinh home

--- app/src/main/java/com/hoanglinhsama/ecommerce/MainActivity.java
+++ app/src/main/java/com/hoanglinhsama/ecommerce/MainActivity.java
@@ -1,14 +1,18 @@
 package com.hoanglinhsama.ecommerce;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hoanglinhsama.ecommerce.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
     }
 }
